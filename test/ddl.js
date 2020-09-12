925ed654a55eb38752d640aa9679357c3f6834cc
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-12 19:52:19 +0300

    feat: read migrations and previous schema from history
    Refs: https://github.com/metarhia/metasql/issues/23

--- test/ddl.js
+++ test/ddl.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const path = require('path');
-const { generate } = require('../lib/ddl.js');
+const { generate, migrate } = require('../lib/ddl.js');
 
 const cwd = process.cwd();
 const schemaPath = path.join(cwd, 'test/schema');
 const outputPath = path.join(cwd, 'test/sql');
 
 generate(schemaPath, outputPath);
+migrate(schemaPath);
