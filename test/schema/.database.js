e18b57f1763a62d7df2c3dd38543ac91a91a81d3
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-12 17:51:00 +0300

    feat: improve migrations structure and example
    Refs: https://github.com/metarhia/metasql/issues/23
    
    PR-URL: https://github.com/metarhia/metasql/pull/25

--- test/schema/.database.js
+++ test/schema/.database.js
@@ -2,6 +2,7 @@
   name: 'example',
   description: 'Example database schema',
   version: 3,
+  driver: 'pg',
 
   authors: [
     { name: 'Timur Shemsedinov', email: 'timur.shemsedinov@gmail.com' },
@@ -12,5 +13,13 @@
     'postgis',
     'postgis_topology',
     'pg_trgm',
-  ]
+  ],
+
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'application',
+    user: 'postgres',
+    password: 'postgres',
+  },
 });
--- test/schema/.history/2020-09-11-v2/.database.js
+++ test/schema/.history/2020-09-11-v2/.database.js
@@ -2,6 +2,7 @@
   name: 'example',
   description: 'Example database schema',
   version: 2,
+  driver: 'pg',
 
   authors: [
     { name: 'Timur Shemsedinov', email: 'timur.shemsedinov@gmail.com' },
@@ -10,5 +11,13 @@
   extensions: [
     'hstore',
     'pg_trgm',
-  ]
+  ],
+
+  connection: {
+    host: '127.0.0.1',
+    port: 5432,
+    database: 'application',
+    user: 'postgres',
+    password: 'postgres',
+  },
 });
