0e6dc59afc2d5147b34a9de20116f6d9d1ec1410
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-15 02:18:49 +0300

    refactor: rename generate to create for pg DDL

--- test/ddl.js
+++ test/ddl.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const path = require('path');
-const { generate, migrate } = require('..');
+const { create, migrate } = require('..');
 
 const cwd = process.cwd();
 const schemaPath = path.join(cwd, 'application/schemas');
 const outputPath = path.join(cwd, 'application/schemas/migrations');
 
-generate(schemaPath, outputPath);
+create(schemaPath, outputPath);
 migrate(schemaPath);
