e7e28d7451439b336acec465607a76ab9002ab0c
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-15 01:39:50 +0300

    refactor: remove dots for history and migrations folders
    PR-URL: https://github.com/metarhia/metasql/pull/33

--- test/ddl.js
+++ test/ddl.js
@@ -5,7 +5,7 @@ const { generate, migrate } = require('..');
 
 const cwd = process.cwd();
 const schemaPath = path.join(cwd, 'application/schemas');
-const outputPath = path.join(cwd, 'application/schemas/.migrations');
+const outputPath = path.join(cwd, 'application/schemas/migrations');
 
 generate(schemaPath, outputPath);
 migrate(schemaPath);
