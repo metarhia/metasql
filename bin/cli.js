1307586bef7aa1525f1e395a7078c35f60993e19
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-09-14 23:43:02 +0300

    feat: add cli stub to project
    Refs: https://github.com/metarhia/metasql/issues/30
    
    PR-URL: https://github.com/metarhia/metasql/pull/33

---
+++ bin/cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const metasql = require('..');
+
+const [,, command, version] = process.argv;
+
+if (command === 'g') {
+  const schemaPath = path.join(__dirname, '../application/schemas');
+  metasql.migrate(schemaPath);
+} else if (command === 'm') {
+  if (version) console.log(`Migrate to version ${version}`);
+  else console.log(`Migrate to the latest version`);
+}
