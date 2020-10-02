5eab1154bbd5abfe0c30092c15e686b5fa824c0e
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-02 22:08:08 +0300

    feat: cli output
    PR-URL: https://github.com/metarhia/metasql/pull/45

--- bin/cli.js
+++ bin/cli.js
@@ -16,5 +16,12 @@ if (command === 'c') {
 } else if (command === 'g') {
   metasql.generate(schemaPath);
 } else if (command === 'm') {
-  metasql.migrate(schemaPath, parseInt(version, 10));
+  const v = version ? parseInt(version, 10) : undefined;
+  metasql.migrate(schemaPath, v);
+} else {
+  console.log('Commands for metasql:');
+  console.log('  metasql c             Create database schema');
+  console.log('  metasql g             Generate migration');
+  console.log('  metasql m             Migrate to the latest version');
+  console.log('  metasql m [version]   Migrate to the specified version');
 }
