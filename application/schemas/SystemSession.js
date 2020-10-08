06bbd45e8a629c4af06411562853971025a61240
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-08 19:41:50 +0300

    feat: add more examples with new schema syntax

---
+++ application/schemas/SystemSession.js
@@ -0,0 +1,6 @@
+({
+  user: 'SystemUser',
+  token: { type: 'string', unique: true },
+  ip: 'string',
+  data: 'json',
+});
---
+++ application/schemas/SystemUser.js
@@ -0,0 +1,5 @@
+({
+  login: { type: 'string', unique: true },
+  password: 'string',
+  fullName: 'string',
+});
