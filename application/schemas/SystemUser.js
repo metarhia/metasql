cc23e07f5a37eca91eda3800ad64b48e13b58263
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-10-12 01:23:05 +0300

    feat: implement { length } attribute metadata
    Closes: https://github.com/metarhia/metasql/issues/57

--- application/schemas/SystemUser.js
+++ application/schemas/SystemUser.js
@@ -1,5 +1,5 @@
 ({
-  login: { type: 'string', unique: true },
-  password: 'string',
-  fullName: 'string',
+  login: { type: 'string', unique: true, length: 30 },
+  password: { type: 'string', length: { min: 10 } },
+  fullName: { type: 'string', length: [5, 60] },
 });
