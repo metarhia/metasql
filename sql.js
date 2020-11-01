44f760b20957c51aadfbcfcfa1c7b54f4a0a7288
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-11-01 04:53:48 +0200

    feat: backport Database class from impress

--- sql.js
+++ sql.js
@@ -1,29 +1,13 @@
 'use strict';
 
-const { QueryBuilder } = require('./lib/query-builder.js');
-const { SelectBuilder } = require('./lib/select-builder.js');
-const { RawBuilder } = require('./lib/raw-builder.js');
-const { ParamsBuilder } = require('./lib/params-builder.js');
-const { PostgresParamsBuilder } = require('./lib/pg-params-builder.js');
+const database = require('./lib/database.js');
 const model = require('./lib/model-db.js');
 const schema = require('./lib/schema-fs.js');
 
 require('./lib/model-pg.js');
 
-const pg = handler => {
-  const params = new PostgresParamsBuilder();
-  const builder = new SelectBuilder(params);
-  if (handler) handler(builder, params);
-  return { builder, params };
-};
-
 module.exports = {
-  QueryBuilder,
-  SelectBuilder,
-  RawBuilder,
-  ParamsBuilder,
-  PostgresParamsBuilder,
-  pg,
+  ...database,
   ...model,
   ...schema,
 };
