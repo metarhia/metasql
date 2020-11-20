746ceedca795100acae88c503e0ce755fd378b38
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-11-20 23:09:18 +0200

    refactor: remove builders

--- types/sql.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/query-builder';
-export * from './lib/raw-builder';
-export * from './lib/select-builder';
-export * from './lib/params-builder';
-export * from './lib/pg-params-builder';
