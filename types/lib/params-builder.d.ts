05a0b12a784d1dcc215eb201ce2cfdfa2f8fbebb
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2020-11-20 23:09:18 +0200

    refactor: remove builders
    PR-URL: https://github.com/metarhia/metasql/pull/88

--- types/lib/params-builder.d.ts
+++
@@ -1,8 +0,0 @@
-export class ParamsBuilder<P = any, O = undefined> {
-  // Add passed value to parameters
-  // Returns a string name to put in an sql query
-  add(value: any, options?: O): string;
-
-  // Generic building method that must return the parameters object
-  build(): P;
-}
--- types/lib/pg-params-builder.d.ts
+++
@@ -1,4 +0,0 @@
-import { ParamsBuilder } from './params-builder';
-
-// Params builder for PostgreSQL database
-export class PostgresParamsBuilder extends ParamsBuilder<Array<any>> {}
