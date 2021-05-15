16492387e2cf1d8e794f26115853f3882c37eabc
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-05-15 03:55:31 +0300

    chore: update metaschema and regenerate ddl
    PR-URL: https://github.com/metarhia/metasql/pull/138

--- application/schemas/database.sql
+++ application/schemas/database.sql
@@ -1,3 +1,19 @@
+CREATE TABLE "Identifier" (
+  "id" bigint generated always as identity,
+  "categoryId" bigint NOT NULL,
+  "storage" varchar NOT NULL,
+  "status" varchar NOT NULL,
+  "creation" timestamp with time zone NOT NULL,
+  "change" timestamp with time zone NOT NULL,
+  "lock" boolean NOT NULL DEFAULT false,
+  "version" integer NOT NULL DEFAULT 0,
+  "hashsum" varchar NOT NULL
+);
+
+ALTER TABLE "Identifier" ADD CONSTRAINT "pkIdentifier" PRIMARY KEY ("id");
+ALTER TABLE "Identifier" ADD CONSTRAINT "fkIdentifierId" FOREIGN KEY ("id") REFERENCES "Identifier" ("id");
+ALTER TABLE "Identifier" ADD CONSTRAINT "fkIdentifierCategory" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id");
+
 CREATE TABLE "Application" (
   "id" bigint generated always as identity,
   "name" varchar NOT NULL
@@ -15,6 +31,14 @@ CREATE TABLE "Unit" (
 ALTER TABLE "Unit" ADD CONSTRAINT "pkUnit" PRIMARY KEY ("id");
 ALTER TABLE "Unit" ADD CONSTRAINT "fkUnitId" FOREIGN KEY ("id") REFERENCES "Identifier" ("id");
 ALTER TABLE "Unit" ADD CONSTRAINT "fkUnitApplication" FOREIGN KEY ("applicationId") REFERENCES "Application" ("id");
+
+CREATE TABLE "Role" (
+  "roleId" bigint generated always as identity,
+  "name" varchar NOT NULL,
+  "blocked" boolean NOT NULL DEFAULT false
+);
+
+ALTER TABLE "Role" ADD CONSTRAINT "pkRole" PRIMARY KEY ("roleId");
 
 CREATE TABLE "Account" (
   "id" bigint generated always as identity,
@@ -77,22 +101,6 @@ ALTER TABLE "Category" ADD CONSTRAINT "pkCategory" PRIMARY KEY ("id");
 ALTER TABLE "Category" ADD CONSTRAINT "fkCategoryId" FOREIGN KEY ("id") REFERENCES "Identifier" ("id");
 ALTER TABLE "Category" ADD CONSTRAINT "fkCategoryApplication" FOREIGN KEY ("applicationId") REFERENCES "Application" ("id");
 
-CREATE TABLE "Identifier" (
-  "id" bigint generated always as identity,
-  "categoryId" bigint NOT NULL,
-  "storage" varchar NOT NULL,
-  "status" varchar NOT NULL,
-  "creation" timestamp with time zone NOT NULL,
-  "change" timestamp with time zone NOT NULL,
-  "lock" boolean NOT NULL DEFAULT false,
-  "version" integer NOT NULL DEFAULT 0,
-  "hashsum" varchar NOT NULL
-);
-
-ALTER TABLE "Identifier" ADD CONSTRAINT "pkIdentifier" PRIMARY KEY ("id");
-ALTER TABLE "Identifier" ADD CONSTRAINT "fkIdentifierId" FOREIGN KEY ("id") REFERENCES "Identifier" ("id");
-ALTER TABLE "Identifier" ADD CONSTRAINT "fkIdentifierCategory" FOREIGN KEY ("categoryId") REFERENCES "Category" ("id");
-
 CREATE TABLE "Server" (
   "id" bigint generated always as identity,
   "name" varchar NOT NULL,
@@ -118,14 +126,6 @@ ALTER TABLE "Journal" ADD CONSTRAINT "pkJournal" PRIMARY KEY ("journalId");
 ALTER TABLE "Journal" ADD CONSTRAINT "fkJournalIdentifier" FOREIGN KEY ("identifierId") REFERENCES "Identifier" ("id");
 ALTER TABLE "Journal" ADD CONSTRAINT "fkJournalAccount" FOREIGN KEY ("accountId") REFERENCES "Account" ("id");
 ALTER TABLE "Journal" ADD CONSTRAINT "fkJournalServer" FOREIGN KEY ("serverId") REFERENCES "Server" ("id");
-
-CREATE TABLE "Role" (
-  "roleId" bigint generated always as identity,
-  "name" varchar NOT NULL,
-  "blocked" boolean NOT NULL DEFAULT false
-);
-
-ALTER TABLE "Role" ADD CONSTRAINT "pkRole" PRIMARY KEY ("roleId");
 
 CREATE TABLE "Permission" (
   "permissionId" bigint generated always as identity,
